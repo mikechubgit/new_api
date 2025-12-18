7ce6aea8c43f6f2d6876d60792804912b4df36ce
unknown <mikecmaher@gmail.com>
2025-12-18 14:13:53 +0000

    remove port number

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn bank_api:app --host 0.0.0.0
+web: uvicorn bank_api:app --host 0.0.0.0 -port 8000
---
+++ client_predicy.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+
+user_input = {"age": 24, "job": "admin.", "marital": "single", "education": "university.degree", "default": "no", "housing": "no", "loan": "no", "contact": "cellular", "month": "aug", "day_of_week": "fri", "campaign": 1, "pdays": 999, "previous": 2, "poutcome": "failure", "emp.var.rate": -1.7, "cons.price.idx": 94.027, "cons.conf.idx": -38.3, "euribor3m": 0.905, "nr.employed": 4991.6}
+
+
+resp = requests.post('http://localhost:8000/predict/', json={'data': user_input})
+
+
+if resp.status_code == 200:
+	result = resp.json()
+	print(result['prediction'])
+	print(result['probability']) 
+	print(result['threshold'])
+else:
+	print(resp.status_code)
+
+
